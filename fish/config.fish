3be78dc4c6816be238f7bbb29b69b00d454f7e51
SchnuBby2205 <faceless666@gmx.de>
2023-04-25 18:28:55 +0200

    First Commit for DotFiles

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias pms="pacman -Ss"
+alias pmi="sudo pacman -S"
+alias pmus="sudo pacman -Syu"
+alias ls="ls -lla"
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/SchnuBby/.local/share/omf/themes/sashimi/fish_prompt.fish
